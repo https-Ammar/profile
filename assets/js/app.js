5dfa619b9a67bb32bb0a2e1e8995d0b83621ae0c
Ammar Ahmed Mostafa <ammarx3@icloud.com>
2025-11-07 18:02:13 +0200

    ED

--- assets/js/app.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.link-btn').forEach(b=>{
-  b.addEventListener('click',()=>{window.open('https://example.com','_blank')})
-})
-document.querySelector('.schedule-btn')?.addEventListener('click',()=>{
-  alert('Open scheduler or add your scheduling link here')
-})
